b59dd2f9df136d9265bd363dc2b997ee54acb010
Mo-olly <60999160+Mo-olly@users.noreply.github.com>
2023-02-13 10:48:38 +0800

    Delete src directory

--- src/network/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export function request(config) {
-    const instance = axios.create({
-        baseURL: 'http://music.cpengx.cn'
-    })
-
-    return instance(config)
-}
