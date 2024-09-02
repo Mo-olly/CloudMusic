c18f5f945b24faf08a598ecf6aced971b0800de1
jiaganyu <jiaganyu2000@126.com>
2024-09-02 17:37:08 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 import Vue3ColorPicker from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
-// import {Swipe, SwipeItem,Lazyload} from 'vant';
-// import 'vant/lib/index.css';
 
-
- //将这个vue3app全局挂载到#app元素上
-const app = createApp(App);   
-app.use(router).use(Vue3ColorPicker).use(store).mount('#app');           
+//将这个vue3app全局挂载到#app元素上
+const app = createApp(App);
+app.use(router).use(Vue3ColorPicker).use(store).mount("#app");
